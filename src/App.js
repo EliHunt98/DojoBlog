ddb8e5c00f5abb94c4782f88dd9eaf0aee77e0a7
Eli Hunt <elihunt98@gmail.com>
2023-09-07 23:45:15 -0600

    Fixing repo issues

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App Component</h1>
+        <p>Test</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
